b36d25dd117d6d5ec37079db5433b1285a691dbf
Aleksandr Danilenko <rorik302@gmail.com>
2025-07-19 18:16:41 +0300

    Установлено home-manager

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: 
@@ -19,6 +24,7 @@
 	  modules = [
 	    ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
 	    ./hosts/default/configuration.nix
+	    inputs.home-manager.nixosModules.default
 	  ];
         };
       };
