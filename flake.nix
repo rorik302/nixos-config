dd50ad3ab4c1a48fc5f626efc60492986b6bed97
Aleksandr Danilenko <rorik302@gmail.com>
2025-07-19 17:51:19 +0300

    Добавлена поддержка unstable ветки

--- flake.nix
+++ flake.nix
@@ -2,17 +2,25 @@
   description = "NixOS configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-	modules = [
-	  ./hosts/default/configuration.nix
-	];
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: 
+    let
+      system = "x86_64-linux";
+      overlay-unstable = final: prev: {
+        unstable = nixpkgs-unstable.legacyPackages.${ prev.system };
+      };
+    in {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+	  modules = [
+	    ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+	    ./hosts/default/configuration.nix
+	  ];
+        };
       };
     };
-  };
 }
