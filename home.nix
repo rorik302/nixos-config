5f91196b124fe2bdfc7008bf87b57b9847100dc3
Aleksandr Danilenko <rorik302@gmail.com>
2025-07-26 11:49:27 +0000

    Добавлена функция y в fish

--- home.nix
+++ home.nix
@@ -13,6 +13,16 @@
     shellInit = ''
       set -g fish_greeting "" 
     '';
+    functions = {
+      y = ''
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+      '';
+    };
   };
 
   programs.neovim = {
@@ -22,6 +32,13 @@
 
   programs.yazi = {
     enable = true;
+    settings = {
+      mgr = {
+	show_hidden = true;
+	sort_by = "natural";
+        linemode = "size";
+      };
+    };
   };
 
   programs.lazygit = {
